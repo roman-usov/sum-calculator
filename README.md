30d00229ccbf1712c86954be2a12437d235d6ffd
roman-usov <66104246+roman-usov@users.noreply.github.com>
2023-07-17 21:37:34 +0400

    completed sum function

---
+++ README.md
@@ -0,0 +1,11 @@
+This project implements a simple sum calculator. It can do so for any amount of numbers. 
+
+![img.png](img.png)
+
+## src/application.js
+
+The app is implemented as a function exported by default. The calculator comprises an input and two buttons: add and reset. The sum is displayed under the input, and is 0 when the app is first launched. Each click on the plus button add the input value to the current sum to get a new sum. The reset button resets the sum to 0.
+
+The input gets focus when the form is rendered and after each submit or reset.
+
+Both submit and reset clear the input.
--- src/application.js
+++ src/application.js
@@ -2,5 +2,43 @@
 // @ts-check
 
 // BEGIN (write your solution here)
+function setFocus(el) {
+  el.focus();
+}
 
+function render(state, formEl, inputEl, resultEl) {
+  resultEl.textContent = state;
+  formEl.reset();
+  setFocus(inputEl);
+}
+
+export default function () {
+  let state = 0;
+  
+  const formEl = document.querySelector('.form-inline');
+  const resetBtn = document.querySelector('.form-inline > button[type="reset"]');
+  const inputEl = document.querySelector('.form-inline > input[type="number"]');
+  const resultEl = document.getElementById('result');
+  
+  setFocus(inputEl);
+  
+  function handlePlus (e) {
+    e.preventDefault();
+    const formElement = e.target;
+    const formData = new FormData(formElement);
+    const submittedInputValue = formData.get('number');
+    const numToSum = submittedInputValue ? Number.parseInt(submittedInputValue, 10) : 0;
+    state += numToSum;
+    render(state, formEl, inputEl, resultEl);
+  }
+  
+  function handleReset(e) {
+    state = 0;
+    render(state, formEl, inputEl, resultEl);
+  }
+  
+  formEl.addEventListener('submit', handlePlus);
+  
+  resetBtn.addEventListener('click', handleReset);
+}
 // END
